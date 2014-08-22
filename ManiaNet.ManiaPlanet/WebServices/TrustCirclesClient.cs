9a27001e5d186838b4cd5e23c091cb69309d27e8
Arne Kiesewetter <banane1996@gmx.net>
2014-08-22 18:38:41 +0200

    Finish TrustCirclesClient

--- ManiaNet.ManiaPlanet/WebServices/TrustCirclesClient.cs
+++ ManiaNet.ManiaPlanet/WebServices/TrustCirclesClient.cs
@@ -24,6 +24,38 @@ namespace ManiaNet.ManiaPlanet.WebServices
         { }
 
         /// <summary>
+        /// Blacklists the Player given by the login, and returns whether it was successful.
+        /// </summary>
+        /// <param name="login">The login of the Player.</param>
+        /// <returns>Whether it was successful.</returns>
+        [UsedImplicitly]
+        public async Task<bool> BlacklistPlayerAsync([NotNull] string login)
+        {
+            if (string.IsNullOrWhiteSpace(login))
+                return false;
+
+            var response = await execute(RequestType.Post, "trust/black/index.txt", login);
+
+            return response != null;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="ListEntry"/>s on the Blacklist of the given Trust Circle. Null when the data couldn't be found.
+        /// </summary>
+        /// <param name="circle">The name of the Trust Circle.</param>
+        /// <returns>The Entries on the Blacklist of the Trust Circle.</returns>
+        [UsedImplicitly]
+        public async Task<ListEntry[]> GetBlacklistAsyncFor([NotNull] string circle)
+        {
+            if (string.IsNullOrWhiteSpace(circle))
+                return null;
+
+            var response = await execute(RequestType.Get, "trust/" + circle + "/black/index.json");
+
+            return response == null ? null : jsonSerializer.Deserialize<ListEntry[]>(new JsonTextReader(new StringReader(response)));
+        }
+
+        /// <summary>
         /// Gets the <see cref="Karma"/> of the given Player in the given Trust Circle. Null when the data couldn't be found.
         /// </summary>
         /// <param name="circle">The name of the Trust Circle.</param>
@@ -37,16 +69,95 @@ namespace ManiaNet.ManiaPlanet.WebServices
 
             var response = await execute(RequestType.Get, "trust/" + circle + "/karma/" + login + "/index.json");
 
-            if (response == null)
+            return response == null ? null : jsonSerializer.Deserialize<Karma>(new JsonTextReader(new StringReader(response)));
+        }
+
+        /// <summary>
+        /// Gets the logins of the Players that are on your own Blacklist. Null when the data couldn't be found.
+        /// </summary>
+        /// <returns>The logins of the Players on your own Blacklist. Null when the data couldn't be found.</returns>
+        [UsedImplicitly]
+        public async Task<string[]> GetOwnBlacklistAsync()
+        {
+            var response = await execute(RequestType.Get, "trust/black/index.json");
+
+            return response == null ? null : jsonSerializer.Deserialize<string[]>(new JsonTextReader(new StringReader(response)));
+        }
+
+        /// <summary>
+        /// Gets the logins of the Players that are on your own Whitelist. Null when the data couldn't be found.
+        /// </summary>
+        /// <returns>The logins of the Players on your own Whitelist. Null when the data couldn't be found.</returns>
+        [UsedImplicitly]
+        public async Task<string[]> GetOwnWhitelistAsync()
+        {
+            var response = await execute(RequestType.Get, "trust/white/index.json");
+
+            return response == null ? null : jsonSerializer.Deserialize<string[]>(new JsonTextReader(new StringReader(response)));
+        }
+
+        /// <summary>
+        /// Gets the <see cref="ListEntry"/>s on the Whitelist of the given Trust Circle. Null when the data couldn't be found.
+        /// </summary>
+        /// <param name="circle">The name of the Trust Circle.</param>
+        /// <returns>The Entries on the Whitelist of the Trust Circle.</returns>
+        [UsedImplicitly]
+        public async Task<ListEntry[]> GetWhitelistAsyncFor([NotNull] string circle)
+        {
+            if (string.IsNullOrWhiteSpace(circle))
                 return null;
 
-            var karma = jsonSerializer.Deserialize<Karma>(new JsonTextReader(new StringReader(response)));
+            var response = await execute(RequestType.Get, "trust/" + circle + "/white/index.json");
 
-            if (karma == null)
-                return null;
+            return response == null ? null : jsonSerializer.Deserialize<ListEntry[]>(new JsonTextReader(new StringReader(response)));
+        }
 
-            karma.GetType().GetProperty("Login").GetSetMethod(true).Invoke(karma, new object[] { login });
-            return karma;
+        /// <summary>
+        /// Unblacklists the Player given by the login, and returns whether it was successful.
+        /// </summary>
+        /// <param name="login">The login of the Player.</param>
+        /// <returns>Whether it was successful.</returns>
+        [UsedImplicitly]
+        public async Task<bool> UnblacklistPlayerAsync([NotNull] string login)
+        {
+            if (string.IsNullOrWhiteSpace(login))
+                return false;
+
+            var response = await execute(RequestType.Post, "trust/unblack/index.txt", login);
+
+            return response != null;
+        }
+
+        /// <summary>
+        /// Unwhitelists the Player given by the login, and returns whether it was successful.
+        /// </summary>
+        /// <param name="login">The login of the Player.</param>
+        /// <returns>Whether it was successful.</returns>
+        [UsedImplicitly]
+        public async Task<bool> UnwhitelistPlayerAsync([NotNull] string login)
+        {
+            if (string.IsNullOrWhiteSpace(login))
+                return false;
+
+            var response = await execute(RequestType.Post, "trust/unwhite/index.txt", login);
+
+            return response != null;
+        }
+
+        /// <summary>
+        /// Whitelists the Player given by the login, and returns whether it was successful.
+        /// </summary>
+        /// <param name="login">The login of the Player.</param>
+        /// <returns>Whether it was successful.</returns>
+        [UsedImplicitly]
+        public async Task<bool> WhitelistPlayerAsync([NotNull] string login)
+        {
+            if (string.IsNullOrWhiteSpace(login))
+                return false;
+
+            var response = await execute(RequestType.Post, "trust/white/index.txt", login);
+
+            return response != null;
         }
 
         /// <summary>
@@ -68,17 +179,6 @@ namespace ManiaNet.ManiaPlanet.WebServices
             }
 
             /// <summary>
-            /// Gets the login of the Player. Won't be null.
-            /// </summary>
-            [NotNull, JsonIgnore, UsedImplicitly]
-            public string Login
-            {
-                get;
-                [UsedImplicitly]
-                private set;
-            }
-
-            /// <summary>
             /// Gets the number of times that the player has been whitelisted in the Trust Circle. May be null if the data wasn't complete.
             /// </summary>
             [JsonProperty("whites"), UsedImplicitly]
@@ -91,6 +191,34 @@ namespace ManiaNet.ManiaPlanet.WebServices
 
             private Karma()
             { }
+        }
+
+        /// <summary>
+        /// Stores informationation about an entry in a Black- or Whitelist.
+        /// </summary>
+        public sealed class ListEntry
+        {
+            /// <summary>
+            /// Gets the number of times that the Player was listed.
+            /// </summary>
+            [JsonProperty("count"), UsedImplicitly]
+            public uint? Count
+            {
+                get;
+                [UsedImplicitly]
+                private set;
+            }
+
+            /// <summary>
+            /// Gets the login of the Player. May be null if the data wasn't complete.
+            /// </summary>
+            [CanBeNull, JsonProperty("login")]
+            public string Login
+            {
+                get;
+                [UsedImplicitly]
+                private set;
+            }
         }
     }
 }
